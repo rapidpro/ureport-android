3f7f24083479ad6b7558d7d848a051ab64f89fe8
John Cordeiro <john.dalton@ilhasoft.com.br>
2015-07-07 12:41:27 -0300

    Add ilhasoft support library for android projects

---
+++ app/src/main/java/in/ureport/fragments/LoginFragment.java
@@ -0,0 +1,26 @@
+package in.ureport.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ilhasoft on 7/7/15.
+ */
+public class LoginFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
