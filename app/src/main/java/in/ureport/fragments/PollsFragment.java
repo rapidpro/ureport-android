c50cfeeb813ee675b6d49d486779181a803eb0ce
John Cordeiro <john.dalton@ilhasoft.com.br>
2016-04-25 16:04:31 -0300

    Update polls results layout name

--- app/src/main/java/in/ureport/fragments/PollsFragment.java
+++ app/src/main/java/in/ureport/fragments/PollsFragment.java
@@ -81,7 +81,7 @@ public class PollsFragment extends Fragment implements PollAdapter.PollParticipa
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_polls, container, false);
+        return inflater.inflate(R.layout.fragment_polls_results, container, false);
     }
 
     @Override
