8ff49653d5578fcf85b3984cfbdcceea387c3f67
Daniel San <danielsan@ilhasoft.com.br>
2017-10-18 12:21:54 -0300

    Add crashlytics
    Signed-off-by: Daniel San <danielsan@ilhasoft.com.br>

--- app/src/main/java/in/ureport/UreportApplication.java
+++ app/src/main/java/in/ureport/UreportApplication.java
@@ -4,8 +4,13 @@ import android.support.multidex.MultiDexApplication;
 
 import com.activeandroid.ActiveAndroid;
 
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import in.ureport.managers.AmazonServicesManager;
 import in.ureport.managers.CountryProgramManager;
@@ -13,6 +18,7 @@ import in.ureport.managers.FirebaseProxyManager;
 import in.ureport.managers.UserManager;
 import in.ureport.services.UreportFcmRegistrationService;
 import io.fabric.sdk.android.Fabric;
+import io.fabric.sdk.android.Kit;
 import io.rapidpro.sdk.FcmClient;
 import io.rapidpro.sdk.UiConfiguration;
 
@@ -46,7 +52,12 @@ public class UreportApplication extends MultiDexApplication {
     }
 
     private void initializeFabric() {
-        TwitterAuthConfig authConfig = new TwitterAuthConfig(getString(R.string.twitter_key), getString(R.string.twitter_secret));
-        Fabric.with(this, new Twitter(authConfig));
+        final Twitter twitter = new Twitter(new TwitterAuthConfig(getString(R.string.twitter_key), getString(R.string.twitter_secret)));
+        if (BuildConfig.DEBUG) {
+            Fabric.with(this, twitter);
+        } else {
+            Fabric.with(this, twitter, new Crashlytics());
+        }
     }
+
 }
