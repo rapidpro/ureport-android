3cd5a6b26a0c31ece5bf435c704c81ae147c60d0
Allan Lima <allan.lima@ilhasoft.com.br>
2018-07-09 14:59:19 -0300

    Fix loading crashes on group picture upload

--- app/src/main/java/in/ureport/fragments/PickMediaFragment.java
+++ app/src/main/java/in/ureport/fragments/PickMediaFragment.java
@@ -1,14 +1,12 @@
 package in.ureport.fragments;
 
 import android.animation.ObjectAnimator;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
